2bc46dc2de47cfe21e659bf50c6038632cc7a959
gwenoleR <gwenole.roton@live.com>
2016-05-25 10:52:50 +0200

    fin tp2

--- main.py
+++ main.py
@@ -45,6 +45,27 @@ def student_average(name):
     resp.headers['Content-Type'] = 'application/json'
     return  resp
 
+@app.route("/topics")
+def topics_list():
+    topics = []
+    for i in range(0,len(students[0]['grades'])):
+        topics.append(students[0]['grades'][i]['topic'])
+    resp = make_response(json.dumps(topics), 200)
+    resp.headers['Content-Type'] = 'application/json'
+    return  resp
+    
+@app.route("/topics/<path:topic>/average")
+def topic_average(topic):
+    average = 0.0
+    for i in range(0, len(students)):
+        for j in range(0, len(students[i]['grades'])):
+            if students[i]['grades'][j]['topic'] == topic :
+                average = average + students[i]['grades'][j]['mark']
+    average = average/len(students)
+    resp = make_response(json.dumps(average), 200)
+    resp.headers['Content-Type'] = 'application/json'
+    return  resp
+
 if __name__ == "__main__":
   app.debug=True
   app.run()
